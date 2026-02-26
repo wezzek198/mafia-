82fd7dda63d959632cb46719be908cf2b2173589
wezzek198 <modbrawl4@gmail.com>
2026-02-26 18:59:04 +0700

    Update app.py

--- mafia_host_bot/src/app.py
+++ mafia_host_bot/src/app.py
@@ -1,19 +1,3 @@
-# Copyright (C) 2017, 2018, 2019, 2020  alfred richardsn
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 import config
 from .logger import logger, log_update
 from .database import database
@@ -130,3 +114,4 @@ def main():
         bot.set_webhook(url=url + config.TOKEN)
     else:
         bot.polling()
+
